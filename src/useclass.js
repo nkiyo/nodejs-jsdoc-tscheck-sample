dff44e766b0365e1da3cd991247b8af1689e7e64
nkiyo <nkiyo@hoge.com>
2020-07-26 13:04:12 +0900

    Update

--- src/useclass.js
+++ src/useclass.js
@@ -12,7 +12,9 @@ const mcs = mc.getS()
  * s: string,
  * n: number,
  * n2: number}
- * | null} 型定義の途中で改行OK, union types も OK */
+ * | null}
+ * 型定義の途中で改行OK, union types も OK
+ * ただし、関数の戻り値の型と合ってなくても警告なし...設定の問題か？ */
 const obj = mc.getJSON(123)
 console.log(`${JSON.stringify(obj)}`)
 
